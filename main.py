6f2f71d9fd04ed5f00a16c25aeeda5308f8e29e9
VanceCup <maksim_ponomarev2003@mail.ru>
2024-08-20 10:56:09 +0500

    Commit DZ

---
+++ main.py
@@ -0,0 +1,10 @@
+#Module 2.2
+first = 5
+second = 4
+third = 6
+if first == second == third :
+    print(3)
+elif first == second or first == third or second == third :
+    print(2)
+elif first != second or second!=third or first != third :
+    print(0)
